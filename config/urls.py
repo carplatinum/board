2ca7f8d8df3fd3d62f0eb8f1e084e89533c39ce4
ANTAQ <mymillions@ya.ru>
2025-11-02 19:42:55 +0300

    править urls

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
-    # Приложения
-    path("api/", include("apps.users.urls")),
-    path("api/", include("apps.ads.urls")),
+    # Приложения с namespace для корректного reverse в тестах и коде
+    path("api/users/", include(("apps.users.urls", "users"), namespace="users")),
+    path("api/ads/", include(("apps.ads.urls", "ads"), namespace="ads")),
 ]
