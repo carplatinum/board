adf4bf8de12a514851fef664ab65f5a36e2efb79
ANTAQ <mymillions@ya.ru>
2025-11-02 18:28:48 +0300

    править ci, env, base

--- config/settings/base.py
+++ config/settings/base.py
@@ -8,9 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
-
 DEBUG = os.getenv("DEBUG", "False") == "True"
-
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 INSTALLED_APPS = [
@@ -65,12 +63,19 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB", "board_db"),
-        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "USER": os.getenv("POSTGRES_USER", "board_user"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "board_password"),
-        "HOST": os.getenv("POSTGRES_HOST", "db"),  # Изменено с localhost на db
+        # По умолчанию для локального докера и продакшена
+        "HOST": os.getenv("POSTGRES_HOST", "db"),
         "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
+
+# При запуске в GitHub Actions изменить хост на localhost для доступа к сервису postgres
+if os.getenv("GITHUB_WORKFLOW") or os.getenv("CI"):
+    DATABASES['default'].update({
+        "HOST": "localhost",
+    })
 
 AUTH_USER_MODEL = "users.User"
 
@@ -105,13 +110,13 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "http://localhost,http://127.0.0.1").split(",")
 
-CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")  # Изменено с localhost на redis
+CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
 CELERY_RESULT_BACKEND = os.getenv("REDIS_URL", "redis://redis:6379/0")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
-CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
